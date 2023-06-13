5fae089c7483d2a79573448850b68626385c16b5
ntzwilly <ntzwilly@gmail.com>
2023-06-13 23:28:48 +0200

    create interface

--- src/index.js
+++ src/index.js
@@ -1,37 +1,81 @@
 const form = document.querySelector("form");
-const input = document.querySelector("input#day");
-const inputError = document.querySelector("input + span.error");
-
-const email = document.getElementById("mail");
-const emailError = document.querySelector("#mail + span.error");
-
-input.addEventListener("input", (event) => {
-  if (input.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    inputError.textContent = ""; // Reset the content of the message
-    inputError.className = "error"; // Reset the visual state of the message
-  } else {
-    // If there is still an error, show the correct error
-    showError();
-  }
-});
+const dayInput = document.querySelector("input#day");
+const monthInput = document.querySelector("input#month");
+const yearInput = document.querySelector("input#year");
+const inputDayError = document.querySelector("input#day + span.error");
+const inputMonthError = document.querySelector("input#month + span.error");
+const inputYearError = document.querySelector("input#year + span.error");
 
 form.addEventListener("submit", (event) => {
-  if (!input.validity.valid) {
-    showError();
+  if (!dayInput.validity.valid) {
+    validateData();
+    console.log("hey my men")
+    event.preventDefault();
+  }
+  if (!monthInput.validity.valid) {
+    validateData();
+    event.preventDefault();
+  }
+  if (!yearInput.validity.valid) {
+    validateData();
     event.preventDefault();
   }
 });
 
-function showError() {
-  if (input.validity.valueMissing) {
-    inputError.textContent = "You need to enter an input address.";
-  } else if (input.validity.typeMismatch) {
-    inputError.textContent = "Entered value needs to be an input address.";
-  } else if (input.validity.tooShort) {
-    inputError.textContent = `input should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
+function validateData(e) {
+  const dayLabel  = document.getElementById("day-label")
+  const monthLabel  = document.getElementById("month-label")
+  const yearLabel  = document.getElementById("year-label")
+  const day = dayInput.value
+  const month = monthInput.value
+  const year = yearInput.value
+  const numberOfDaysInMonth = new Date(yearInput, monthInput, 0)
+  console.log(numberOfDaysInMonth)
+  
+  if (dayInput.validity.valueMissing) {
+    inputDayError.textContent = "This field is required";
+    dayLabel.style.color = "hsl(0, 100%, 67%)"
+    dayInput.style.borderColor = "hsl(0, 100%, 67%)"
+  }
+  else if (day > numberOfDaysInMonth.getDate()) {
+    inputDayError.textContent = "The date must be valid";
+    dayLabel.style.color = "hsl(0, 100%, 67%)"
+    dayInput.style.borderColor = "hsl(0, 100%, 67%)"
+  }else {
+    inputDayError.textContent = "";
+    dayLabel.style.color = "hsl(0, 1%, 44%)"
+    dayInput.style.borderColor = "hsl(0, 1%, 44%)"
+    checkValidity()
   }
 
-  inputError.className = "error active";
+  if (monthInput.validity.valueMissing) {
+    inputMonthError.textContent = "This field is required";
+    monthLabel.style.color = "hsl(0, 100%, 67%)"
+    monthInput.style.borderColor = "hsl(0, 100%, 67%)"
+  }else {
+    inputMonthError.textContent = "";
+    monthLabel.style.color = "hsl(0, 1%, 44%)"
+    monthInput.style.borderColor = "hsl(0, 1%, 44%)"    
+  }
+  
+  if (yearInput.validity.valueMissing) {
+    inputYearError.textContent = "This field is required";
+    yearLabel.style.color = "hsl(0, 100%, 67%)"
+    yearInput.style.borderColor = "hsl(0, 100%, 67%)"
+  }else {
+    inputYearError.textContent = "";
+    yearLabel.style.color = "hsl(0, 1%, 44%)"
+    yearInput.style.borderColor = "hsl(0, 1%, 44%)"    
+  }
 }
+
+// function checkValidity() {
+//   const day = dayInput.value
+//   const month = monthInput.value
+//   const year = yearInput.value
+//   const numberOfDaysInMonth = new Date(year, month, 0)
+//   if(day > numberOfDaysInMonth.getDate()) {
+//     const a = dayInput.setCustomValidity("This field in not valid")
+//     console.log(a) 
+//   }
+// }
